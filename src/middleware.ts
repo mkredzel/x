67e7d06f8de618b5572a09d27fe943ea936cca16
Marcel Kredzel <marcel.kredzel@gmail.com>
2023-10-07 01:29:15 +0100

    Add auth

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware();
+
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,18 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const MyApp: AppType = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider  {...pageProps}>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
